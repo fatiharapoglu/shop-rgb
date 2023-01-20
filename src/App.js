a103153233a930472880f677758a03713b944622
Fatih Arapoglu <f3tt4n@gmail.com>
2023-01-20 18:56:38 +0300

    add router skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/c-home";
+import Shop from "./components/c-shop";
+import About from "./components/c-about";
+import Nav from "./components/m-nav";
 
 const App = () => {
-    return <div>null</div>;
+    return (
+        <>
+            <Nav />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </>
+    );
 };
 
 export default App;
---
+++ src/components/m-nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="navbar-left">
+                myShop
+            </Link>
+            <ul className="navbar-right">
+                <li>
+                    <NavLink to="/shop">Shop</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
