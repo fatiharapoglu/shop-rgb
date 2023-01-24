c71f55fd29d254bd3ab2498455fcd0769190d1ed
Fatih Arapoglu <f3tt4n@gmail.com>
2023-01-24 21:04:20 +0300

    add homepage rtx image, improve stylings

--- src/components/c-home.js
+++ src/components/c-home.js
@@ -2,6 +2,7 @@ import React from "react";
 import video from "../assets/background.mp4";
 import { NavLink } from "react-router-dom";
 import Footer from "./m-footer";
+import rtx from "../assets/4090.png";
 
 const Home = () => {
     return (
@@ -14,17 +15,22 @@ const Home = () => {
                     <span className="slogan">Where technology meets color.</span>
                 </div>
                 <div className="homepage-header-right">
-                    <NavLink to="/shop">SHOP</NavLink>
-                    <span>Bag</span>
+                    <NavLink to="/shop">Shop</NavLink>
+                    <button className="btn">Bag</button>
                 </div>
             </header>
-            <main className="homepage-main">
-                <h1>GeForce RTX 40 Series</h1>
-                <h2>BEYOND FAST</h2>
-                <p>NVIDIA® GeForce RTX™ 40 Series GPUs are now available.</p>
-                <NavLink to="/shop">
-                    <button className="btn">See All Products</button>
-                </NavLink>
+            <main className="homepage-main-wrapper">
+                <div className="homepage-main">
+                    <h1>GeForce RTX 40 Series</h1>
+                    <h2>BEYOND FAST</h2>
+                    <p>NVIDIA® GeForce RTX™ 40 Series GPUs are now available.</p>
+                    <NavLink to="/shop">
+                        <button className="btn">See All Products</button>
+                    </NavLink>
+                </div>
+                <div className="main-image">
+                    <img src={rtx} alt="rtx 4090" />
+                </div>
             </main>
             <video className="homepage-background" autoPlay muted loop>
                 <source src={video} type="video/mp4" />
