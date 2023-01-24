80a59fdf6e83ee7dec33ee899f3228fa04fc9b01
Fatih Arapoglu <f3tt4n@gmail.com>
2023-01-25 00:40:21 +0300

    organize components

--- src/components/c-home.js
+++ src/components/c-home.js
@@ -3,26 +3,12 @@ import video from "../assets/background.mp4";
 import { NavLink } from "react-router-dom";
 import Footer from "./m-footer";
 import rtx from "../assets/4090.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import Nav from "./m-nav";
 
 const Home = () => {
     return (
         <div className="homepage">
-            <header className="homepage-header">
-                <div className="homepage-header-left">
-                    <span className="brand">
-                        RGB <span>PC</span>
-                    </span>
-                    <span className="slogan">Where technology meets color.</span>
-                </div>
-                <div className="homepage-header-right">
-                    <NavLink to="/shop">Shop</NavLink>
-                    <button className="btn">
-                        <FontAwesomeIcon icon={faCartShopping} />
-                    </button>
-                </div>
-            </header>
+            <Nav />
             <main className="homepage-main-wrapper">
                 <div className="homepage-main">
                     <h1>GeForce RTX 40 Series</h1>
--- src/components/m-nav.js
+++ src/components/m-nav.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
     return (
-        <nav className="navbar">
-            <Link to="/" className="navbar-left">
-                EliteTechWorks
-            </Link>
-            <ul className="navbar-right">
-                <li>
-                    <NavLink to="/shop">Shop</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <header className="navbar">
+            <div className="navbar-left">
+                <Link to="/">
+                    <span className="brand">
+                        RGB <span>PC</span>
+                    </span>
+                </Link>
+                <span className="slogan">Where technology meets color.</span>
+            </div>
+            <div className="navbar-right">
+                <NavLink to="/shop">Shop</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <button className="btn">
+                    <FontAwesomeIcon icon={faCartShopping} />
+                </button>
+            </div>
+        </header>
     );
 };
 
